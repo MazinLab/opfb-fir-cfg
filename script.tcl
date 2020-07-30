d192fc9057c7a9acb26be4c36a358655a1233ca9
Jenny Smith <jennifer_smith@ucsb.edu>
2020-07-29 17:44:15 -0700

    updated run script for batch mode

--- script.tcl
+++ script.tcl
@@ -1,14 +1,28 @@
-open_project opfb-fir-cfg
-set_top opfb_fir_cfg
+# Create project
+open_project -reset opfb-fir-cfg
+
+# Add sources files and test bench
 add_files src/fir_config.cpp
 add_files src/fir_config.hpp
 add_files src/fir_confighelp.cpp
 add_files -tb src/tb.cpp -cflags "-Wno-unknown-pragmas"
-open_solution "solution1"
+
+# Specify the top-level function for synthesis
+set_top opfb_fir_cfg
+
+# Create solution1
+open_solution -reset "solution1"
+
+# Specify Xilinx part, clock period, and export style
 set_part {xczu28dr-ffvg1517-2-e}
 create_clock -period 550MHz -name default
-config_export -description {Generate channel config stream for OPFB FIRs} -display_name "OPFB FIR Config Gen" -format ip_catalog -library mkidgen3 -rtl verilog -vendor MazinLab -version 0.1
-csim_design
-#csynth_design
-#cosim_design -tool xsim
+config_export -description {Generate channel config stream for OPFB FIRs} -display_name "OPFB FIR Config Gen" -format ip_catalog -library mkidgen3 -rtl verilog -vendor MazinLab -version 0.2
 
+# Simulate the C Code
+csim_design
+
+# Synethsize, verify the RTL, and produce IP
+csynth_design
+cosim_design -tool xsim
+export_design
+exit
